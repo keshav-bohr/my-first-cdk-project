8448a4f7ec070a0be62244d2f75fe751e11ded7a
Keshav Bohra <keshav2016@gmail.com>
2024-01-18 22:06:03 +0530

    aws bootstrap and deployed s3 bucket

--- lib/my-first-cdk-project-stack.ts
+++ lib/my-first-cdk-project-stack.ts
@@ -1,16 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 
 export class MyFirstCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyFirstCdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    });
   }
 }
